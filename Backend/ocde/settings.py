5d20b46a4fe8935ed65b9aaf6b9635b65fac38b7
190050103 <sahilsuman@cse.iitb.ac.in>
2020-12-08 22:42:58 +0530

    code editor-raja

--- Backend/ocde/settings.py
+++ Backend/ocde/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cleanup.apps.CleanupConfig',
+    'bootstrap',
+    'fontawesome'
 ]
 
 MIDDLEWARE = [
